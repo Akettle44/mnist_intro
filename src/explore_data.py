917db9da47ae88a440fe66a4796b72a5737ed1ef
akettle44 <andrewjkettle@gmail.com>
2021-06-18 14:13:31 -0700

    Tried a few more hyperparams, overall decently happy with accuracy

--- src/explore_data.py
+++ src/explore_data.py
@@ -52,15 +52,11 @@ def setup_data(train, test):
 
 	model = models.Sequential()
 
-	#play around with the layer values later 
-	model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-	#model.add(layers.Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=(28, 28, 1)))
+	model.add(layers.Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=(28, 28, 1)))
 	model.add(layers.MaxPooling2D((2, 2)))
-	model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-	#model.add(layers.Conv2D(64, (3, 3), padding="same", activation='relu'))
+	model.add(layers.Conv2D(64, (3, 3), padding="same", activation='relu'))
 	model.add(layers.MaxPooling2D((2, 2)))
-	#model.add(layers.Conv2D(64, (3, 3), padding="same", activation='relu'))
-	model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+	model.add(layers.Conv2D(64, (3, 3), padding="same", activation='relu'))
 
 	model.add(layers.Flatten())
 	model.add(layers.Dense(64, activation='relu'))
